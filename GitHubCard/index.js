798934bd417a7a16d805010b8e3d7dee650471d5
Seth MacPherson <macpherson.seth@gmail.com>
2020-10-09 18:14:45 -0700

    Record speed - I finished this assignment WAY faster than I expected.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,9 +1,17 @@
+let destination_div = document.querySelector(".cards")
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
+axios.get("https://api.github.com/users/SethMacP")
+  .then(result => {
+    const gitData = result.data;
+    console.log("gitData", gitData);
+    const newCard = makeACard(gitData);
+    destination_div.appendChild(newCard);
+  })
+  .catch (err => console.log("error", err))
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +36,30 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+]
+let follower_data_array = [];
+
+
+//old code
+followersArray.forEach((ele)=>{
+  axios.get("https://api.github.com/users/" + ele)
+    .then((res)=>{
+      destination_div.appendChild(makeACard(res.data))
+    })
+})
+// //Error checking my forEach
+// for (let i = 0 ; i < followersArray.length ; i++){
+//   axios.get("https://api.github.com/users/" + followersArray[i])
+//     .then((res)=>{
+//       destination_div.appendChild(makeACard(res.data));
+//     })
+// }
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -50,6 +81,70 @@ const followersArray = [];
     </div>
 */
 
+
+
+const makeACard = (obj) => {
+  //Console Log Holder
+  console.log("1 Log" , );
+  console.log("2 Log" , );
+  console.log("3 Log" , );
+ 
+  
+  
+  //necessary selectors
+  let destination_div = document.querySelector(".cards")
+  //create all elements
+  const card_div_parent = document.createElement("div");
+  const card_img = document.createElement("img");
+  const card_div_child = document.createElement("div");
+  const card_h3 = document.createElement("h3");
+  //THOUGHT - Add all P variable names to array and do .forEach for createElement
+  const card_p_username = document.createElement("p");
+  const card_p_location = document.createElement("p");
+  const card_p_profile = document.createElement("p");
+  const card_a = document.createElement("a");
+  const card_p_followers = document.createElement("p");
+  const card_p_following = document.createElement("p");
+  const card_p_bio = document.createElement("p");
+
+  //classlist Add
+  card_div_parent.classList.add("card");
+  card_div_child.classList.add("card-info");
+  card_h3.classList.add("name");
+  card_p_username.classList.add("username");
+
+
+  //set content of the nodes
+card_img.src = obj.avatar_url;
+card_h3.textContent = obj["name"];
+card_p_username.textContent = obj["login"];
+card_p_location.textContent = `Location: ${obj["location"]}`;
+card_p_profile.textContent = "Profile:"
+card_a.href = obj.url;
+card_p_followers.textContent = `Followers: ${obj["followers"]}`;
+card_p_following.textContent = `Following: ${obj["following"]}`;
+card_p_bio.textContent = `Bio: ${obj["bio"]}`;
+
+  //append all elements
+  //Contents of the parent div
+  card_div_parent.appendChild(card_img);
+  card_div_parent.appendChild(card_div_child);
+  //Contents of child div
+  card_div_child.appendChild(card_h3);
+  card_div_child.appendChild(card_p_username);
+  card_div_child.appendChild(card_p_location);
+  card_div_child.appendChild(card_p_profile);
+  card_p_profile.appendChild(card_a);
+  card_div_child.appendChild(card_p_followers);
+  card_div_child.appendChild(card_p_following);
+  card_div_child.appendChild(card_p_bio);
+  //content appended to page
+  destination_div.appendChild(card_div_parent);
+  //return statement
+  return card_div_parent;
+  
+}
+// makeACard ( premadeData ) ;
 /*
   List of LS Instructors Github username's:
     tetondan
@@ -58,3 +153,8 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+
+//example data from github
+
+//This is what my api pull showed the first time. 
